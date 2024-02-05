8ac3de201bc03b0cca8f5699290e12afd75a1ec5
Rafael Couto <couto.rafael@gmail.com>
2024-02-05 16:44:36 -0300

    estilizando

--- procedimento1/ordenando.js
+++ procedimento1/ordenando.js
@@ -1,3 +1,65 @@
+const swap = (vetor, position1, position2) => {
+  [vetor[position1], vetor[position2]] = [vetor[position2], vetor[position1]];
+};
+
+const shuffle = (vetor, numberOfSwaps) => {
+  for (let i = 0; i < numberOfSwaps; i++) {
+    const indexRandom1 = Math.floor(Math.random() * vetor.length);
+    const indexRandom2 = Math.floor(Math.random() * vetor.length);
+    swap(vetor, indexRandom1, indexRandom2);
+  }
+};
+
+const bubble_sort = (vetor) => {
+  const leng = vetor.length;
+  for (let i = 0; i < leng - 1; i++) {
+    for (let x = 0; x < leng - i - 1; x++) {
+      if (vetor[x] > vetor[x + 1]) {
+        swap(vetor, x, x + 1);
+      }
+    }
+  }
+};
+
+const selection_sort = (vetor) => {
+  const leng = vetor.length;
+  for (let i = 0; i < leng - 1; i++) {
+    let minimumIndex = i;
+    for (let x = i + 1; x < leng; x++) {
+      if (vetor[x] < vetor[minimumIndex]) {
+        minimumIndex = x;
+      }
+    }
+    if (minimumIndex !== i) {
+      swap(vetor, i, minimumIndex);
+    }
+  }
+  return vetor;
+};
+
+const quick_sort = (vetor, start, end) => {
+  if (start < end) {
+    const indexPivot = particionamento(vetor, start, end);
+    quick_sort(vetor, start, indexPivot - 1);
+    quick_sort(vetor, indexPivot + 1, end);
+  }
+};
+
+const particionamento = (vetor, start, end) => {
+  const pivot = vetor[end];
+  let i = start - 1;
+
+  for (let x = start; x < end; x++) {
+    if (vetor[x] < pivot) {
+      i++;
+      swap(vetor, i, x);
+    }
+  }
+
+  swap(vetor, i + 1, end);
+  return i + 1;
+};
+
 function add() {
   //Capturar o campo de entrada
   var inputValue = document.getElementById("valor");
@@ -19,6 +81,54 @@ function add() {
   inputValue.value = "";
 }
 
-function ordenar() {}
+function ordenar() {
+  //Capturar a lista
+  var valueList = document.getElementById("valores");
+  var selectionList = document.getElementById("algoritmo");
 
-function misturar() {}
+  //cada nó da lista de valores e adicionar
+  var valoresVetor = Array.from(valueList.children).map((item) =>
+    parseInt(item.innerHTML)
+  );
+
+  //Escolher o algoritmo
+  var algoritmoSelecionado =
+    selectionList.options[selectionList.selectedIndex].value;
+
+  if (algoritmoSelecionado === "bubble-sort") {
+    bubble_sort(valoresVetor);
+  } else if (algoritmoSelecionado === "selection-sort") {
+    selection_sort(valoresVetor);
+  } else if (algoritmoSelecionado === "quick-sort") {
+    quick_sort(valoresVetor, 0, valoresVetor.length - 1);
+  }
+
+  //Map e Reduce para novos itens
+  const newItens = valoresVetor
+    .map((valor) => `<li>${valor}</li>`)
+    .reduce((acc, item) => acc + item, "");
+
+  //Substituir o conteúdo da lista - innerHTML
+  valueList.innerHTML = newItens;
+}
+
+function misturar() {
+  //Capturar a lista
+  var valueList = document.getElementById("valores");
+
+  //cada nó da lista de valores e adicionar
+  var valoresVetor = Array.from(valueList.children).map((item) =>
+    parseInt(item.innerHTML)
+  );
+
+  //Aplicar shuffle
+  shuffle(valoresVetor, 5);
+
+  //Map e Reduce
+  const newItens = valoresVetor
+    .map((valor) => `<li>${valor}</li>`)
+    .reduce((acc, item) => acc + item, "");
+
+  //Substituir o conteúdo
+  valueList.innerHTML = newItens;
+}
